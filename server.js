6e225cbf7311f86270716908b3b71222af519926
FX-Wood <fxwood2@gmail.com>
2019-02-15 10:17:32 -0800

    adding header/footer

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const app = express();
 
+const archer = {
+    name: "Sterling Archer",
+    obsessions: [
+        'spying',
+        'sarcasm',
+        'Kenny Loggins',
+        'deep-rooted emotional problems'
+    ]
+}
+
 // app.set and app.get are used for configuring
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/static'));
 
 app.get("/", function(req, res) {
-    res.render('index', {name: "Sterling Archer"});
+    res.render('index', archer);
 })
+
+app.listen(3000);
